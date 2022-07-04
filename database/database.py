bcc1a0d982fae5632b59b976e3470c2944ebd15f
natanimn <108242613+natanimn@users.noreply.github.com>
2022-07-04 16:10:44 +0300

    database changed from mysql to postgresql :)

--- database/database.py
+++ database/database.py
@@ -1,29 +1,24 @@
-from mysql import connector
+from psycopg2 import connect
 import os
 
 def connection():
-    conn = connector.connect(
-            password=os.getenv('pwd'),
-            user=os.getenv('user'),
-            database=os.getenv('database'),
-            host=os.getenv('host'),
-            port=int(os.getenv('port'))
+
+    conn = connect(
+            password="@Nati",
+            user="postgres",
+            database="learningpagebot",
+            host='localhost',
+            port=5432
         )
-    try:
-        conn.connect(buffered=True)
-        conn.autocommit = True
-        
-    except:
-        conn.reconnect(buffered=True)
-        conn.autocommit = True
-    
-    else:
-        return conn
+
+    conn.autocommit = True
+    return conn
+
 
 class PrivateDatabase:
     def __init__(self):
         conn = connection()
-        cur = conn.cursor(buffered=True)
+        cur = conn.cursor()
         cur.execute("select msg_id from books")
         bi = cur.fetchone()
         sub = ['math', 'physics', 'chemistry', 'biology', 'civics', 'geography', 'ict',  'hpe',  'history', 'english', 'amharic']
@@ -35,6 +30,7 @@ class PrivateDatabase:
                         (grade, i, s, 0, 0))
                         conn.commit()
 
+
     def update_query(self, query, *args):
         conn = connection()
         cur = conn.cursor()
@@ -44,6 +40,7 @@ class PrivateDatabase:
             cur.execute(query, args)
         conn.commit()
     
+
     def select_query(self, query, *args):
         conn = connection()
         cur = conn.cursor()
@@ -53,40 +50,37 @@ class PrivateDatabase:
             cur.execute(query, args)
         return cur
         
+
     def user_is_not_exist(self, user_id):
         conn = connection()
-        cur = conn.cursor(buffered=True)
+        cur = conn.cursor()
         cur.execute("SELECT user_id FROM students")
-
         if not user_id in [i for j in cur.fetchall() for i in j]:
                 return True
         else:
             return False 
 
-    def save_data(self, first_name, user_id, date,
+
+    def save_data(self, name, user_id, date,
                 link, acc_lick, status):
                     
-        list = [first_name, user_id, date, link, acc_lick, '', 'No bio yet', status]
+        list = [name, user_id, date, link, acc_lick, status]
         self.update_query('''    
             INSERT INTO students(
-            first_name, 
+            name, 
             user_id,
             joined_date,
             invitation_link, 
-            account_link,
-            withdraw,
-            gender, 
-            bio,
+            account_link, 
             status)
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            VALUES (%s,%s,%s,%s,%s,%s)
             ''', *list)
 
     def update_lang(self, lang, user_id):
         self.update_query("""UPDATE students SET lang = %s WHERE user_id = %s""", lang, user_id)
-        
 
-    def update_name(self, user_id, first_name):
-        self.update_query("UPDATE students SET first_name = %s WHERE user_id = %s", first_name, user_id)
+    def update_name(self, user_id, name):
+        self.update_query("UPDATE students SET name = %s WHERE user_id = %s", name, user_id)
         
     def update_username(self, user_id, username):
         self.update_query("UPDATE students SET username = %s WHERE user_id = %s", username, user_id)
