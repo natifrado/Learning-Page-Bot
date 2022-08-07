b20fd3debab7df787327e5aed0dc3d2ba18bee83
natanimn <108242613+natanimn@users.noreply.github.com>
2022-08-07 17:21:00 +0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -228,11 +228,11 @@ def members_button(max_id: int, curret_row: int):
                 btn_list.append(types.InlineKeyboardButton(f"⏩ {row_}" if curret_row + 5 <= row_ else f"▶ {row_}",
                                                            callback_data=f'members_{row_}'))
         if not curret_row == row_+1 and left:
-            if not row_+1 == 2:
+            if True:
                 btn_list.append(types.InlineKeyboardButton(f"⏩ {row_ + 1}" if curret_row + 5 <= row_ else f"▶ {row_ + 1}",
                                                         callback_data=f'members_{row_ + 1}'))
         if not curret_row == row_ and not left:
-            if not row_+1 == 2:
+            if True:
                 btn_list.append(types.InlineKeyboardButton(f"⏩ {row_}" if curret_row + 5 <= row_ else f"▶ {row_}",
                                                        callback_data=f'members_{row_}'))
     btn.add(*btn_list)
